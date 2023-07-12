1b6507ff3e9662b2d84b5e94c1364d7e87d9852b
Eli Schickler <eli@elis-air-2.devices.brown.edu>
2023-07-12 11:57:37 -0400

    Revised version of prolog final project

--- finalproject.pl
+++ finalproject.pl
@@ -1,31 +1,41 @@
-seasonAll(fall, [CS15, CS17, CS141, CS126]).
-seasonAll(spring, [CS16, CS18, CS22, CS32, CS166]).
+seasonAll(fall, [cs15, cs17, cs141, cs126]).
+seasonAll(spring, [cs16, cs18, cs22, cs32, cs166]).
 
 season(Term, Class) :-
     seasonAll(Term, Classes),
     member(Class, Classes).
 
-levelAll(intro, [CS15, CS17, CS16, CS18]).
-levelAll(intermediate, [CS33, CS32, CS22]).
-levelAll(upper, [CS141, CS166, CS126]).
+levelAll(intro, [cs15, cs17, cs16, cs18]).
+levelAll(intermediate, [cs33, cs32, cs22]).
+levelAll(upper, [cs141, cs166, cs126]).
 
 level(Difficulty, Class) :-
     levelAll(Difficulty, Classes),
     member(Class, Classes).
 
-before(CS16, CS15).
-before(CS18, CS17).
-beforeAll(CS33, [CS16, CS18]).
-beforeAll(CS32, [CS16, CS18]).
-before(CS22, none)
-beforeAll(CS141, [CS16, CS18, CS22]).
-before(CS166, CS33).
-beforeAll(CS126, [CS22, CS32]).
-beforeAll(none, [CS15, CS17, CS22]).
+before(cs16, cs15).
+before(cs18, cs17).
+beforeAll(cs33, [cs16, cs18]).
+beforeAll(cs32, [cs16, cs18]).
+before(cs22, none).
+beforeAll(cs141, [cs16, cs18, cs22]).
+before(cs166, cs33).
+beforeAll(cs126, [cs22, cs32]).
+beforeAll(none, [cs15, cs17, cs22]).
 
 before(Class, Prereq) :-
     beforeAll(Class, Prereqs),
     member(Prereq, Prereqs).
+
+
+studentAll(mark, [cs15, cs16, cs17, cs18, cs32]).
+studentAll(elon, [cs15, cs16, cs17, cs22, cs141, cs33, cs166]).
+studentAll(sherylsandberg, [cs15, cs17, cs16, cs18, cs33, cs32, cs22]).
+studentAll(jeffbezos, [cs15, cs17, cs16, cs18, cs33, cs22, cs141, cs166]).
+
+student(Name, Class) :-
+    studentAll(Name, Classes),
+    member(Class, Classes).
 
 fall(Class) :-
     season(fall, Class).
@@ -48,31 +58,26 @@ intermediate(Class) :-
 upper_level(Class) :-
     level(upper, Class).
 
-can_take(Name, Class) :-
-    class_has_no_prereqs(Class);
-    doublereq(Name, Class);
-    (has_prereqs(Class, Prereq),
-    has_taken(Name, Prereq),
-    Class \== CS126).
-
-class_has_no_prereqs(Class) :-
-    no_prereqs(Class);
-    (has_prereqs(Class, Prereq),
-    class_has_no_prereqs(Prereq)). 
-
 has_taken(Name, Class) :-
     student(Name, Class).
 
-StudentAll(mark, [CS15, CS16, CS17, CS18, CS32]).
-StudentAll(elon, [CS15, CS16, CS17, CS22, CS141, CS33, CS166]).
-StudentAll(sherylsandberg, [CS15, CS17, CS16, CS18, CS33, CS32, CS22]).
-StudentAll(jeffbezos, [CS15, CS17, CS16, CS18, CS33, CS22, CS141, CS166]).
-
-student(Name, Class) :-
-    studentAll(Name, Classes),
-    member(Class, Classes).
-
 doublereq(Name, Class) :-
-    has_taken(Name, CS22),
-    has_taken(Name, CS32),
-    Class == CS126.
+    (has_taken(Name, cs22),
+    has_taken(Name, cs32),
+    Class == cs126);
+    (has_taken(Name, cs22),
+    (has_taken(Name, cs16);
+    has_taken(Name, cs18)),
+    Class == cs141).
+
+can_take(Name, Class) :-
+    no_prereqs(Class);
+    doublereq(Name, Class);
+    (has_prereqs(Class, Prereq),
+    has_taken(Name, Prereq),
+    Class \== cs126,
+    Class \== cs141).
+
+
+
+
